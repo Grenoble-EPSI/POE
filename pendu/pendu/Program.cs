394e96b7831a9a0e97918007da0821c0d26002ea
Celine Cepeda <celine_cepeda@hotmail.fr>
2018-07-09 11:01:06 +0200

    Test si lettre présente dans le mot

--- pendu/pendu/Program.cs
+++ pendu/pendu/Program.cs
@@ -7,6 +7,22 @@ namespace pendu
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Lettre pste ds mot
+
+            string mot = "fox";
+            string caratereTape = "f";
+            if (mot.Contains(caratereTape))
+            {
+                Console.WriteLine($"Bravo the charactere {caratereTape} is in the word");
+            }
+            else
+            {
+                Console.WriteLine("You are wrong, try again");
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
