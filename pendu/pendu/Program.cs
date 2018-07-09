4061b3a0e3057c9525737eb879b2571467acf9e3
Joffrey <joffrey.garo@gmail.com>
2018-07-09 10:40:09 +0200

    Ajout boucle jeu

--- pendu/pendu/Program.cs
+++ pendu/pendu/Program.cs
@@ -7,6 +7,18 @@ namespace pendu
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            while (!IsOver())
+            {
+
+
+            }
+
+        }
+
+        private static bool IsOver()
+        {
+            return false;
         }
     }
 }
