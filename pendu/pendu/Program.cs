b10c2b82dbd1daa7cc01c8ddee939fd16e029474
ssheikhi <32467572+ssheikhi@users.noreply.github.com>
2018-07-09 10:55:44 +0200

    Test si Lettre presente dans le mot choisi est fait

--- pendu/pendu/Program.cs
+++ pendu/pendu/Program.cs
@@ -7,6 +7,17 @@ namespace pendu
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            
+            string mot = "fox";
+            string caractereTape =  "f";
+
+            if (mot.Contains(caractereTape))
+            {
+                Console.WriteLine("ok");
+            }
+            else Console.WriteLine(" not ok");
+            Console.ReadKey();
         }
     }
 }
